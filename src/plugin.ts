169cc319df44a9c2efccd9f967e5b1112da3246e
Mikael Karon <mikael@karon.se>
2023-07-05 22:50:55 +0200

    fix: don't leak `decorator` outside plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -10,22 +10,26 @@ export interface FastifyOpenIDClientPluginOptions {
 
 export const openIDClientPlugin: FastifyPluginAsync<FastifyOpenIDClientPluginOptions> =
   fp(
-    async (fastify, options) => {
-      const issuer = await openIDCreateIssuer.call(fastify, options.issuer)
+    async (fastify, {
+      issuer: { decorator: issuerDecorator, ...createIssuerOptions },
+      ...options
+    }) => {
+      const issuer = await openIDCreateIssuer.call(fastify, createIssuerOptions)
       fastify.log.debug(issuer.metadata, 'OpenID issuer metadata')
-      if (options.issuer.decorator !== undefined) {
+      if (issuerDecorator !== undefined) {
         fastify.log.trace(
-          `decorating \`fastify[${String(options.issuer.decorator)}]\` with OpenID issuer`
+          `decorating \`fastify[${String(issuerDecorator)}]\` with OpenID issuer`
         )
-        fastify.decorate(options.issuer.decorator, issuer)
+        fastify.decorate(issuerDecorator, issuer)
       }
       if (options.client !== undefined) {
-        const client = openIDCreateClient.call(fastify, issuer, options.client)
+        const { decorator: clientDecorator, ...createClientOptions } = options.client
+        const client = openIDCreateClient.call(fastify, issuer, createClientOptions)
         fastify.log.debug(client.metadata, 'OpenID client metadata')
         fastify.log.trace(
-          `decorating \`fastify[${String(options.client.decorator)}]\` with OpenID client`
+          `decorating \`fastify[${String(clientDecorator)}]\` with OpenID client`
         )
-        fastify.decorate(options.client.decorator, client)
+        fastify.decorate(clientDecorator, client)
       }
     },
     {
