7ee7cd59208c4e67ec3a240fee7204e04446bd4c
Mikael Karon <mikael@karon.se>
2024-02-20 16:34:18 +0100

    feat: export `openIDClientPlugin` and `fp(openIDClientPlugin)` separately for `fastify-plugin@4.5.1` compatibility.
    BREAKING CHANGE: This changes the default export for the plugin. Should be safe to upgrade for most users that use the `default` export.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,41 +1,37 @@
-import { type FastifyPluginAsync } from 'fastify'
+import { type FastifyPluginAsync, type FastifyPluginOptions } from 'fastify'
 import fp from 'fastify-plugin'
 import { openIDCreateClient, type OpenIDCreateClientOptions } from './client.js'
 import { openIDCreateIssuer, type OpenIDCreateIssuerOptions } from './issuer.js'
 
-export interface FastifyOpenIDClientPluginOptions {
+export type FastifyOpenIDClientPluginOptions = FastifyPluginOptions & {
   issuer: { decorator?: string | symbol } & OpenIDCreateIssuerOptions
   client?: { decorator: string | symbol } & OpenIDCreateClientOptions
 }
 
-export const openIDClientPlugin: FastifyPluginAsync<FastifyOpenIDClientPluginOptions> =
-  fp(
-    async (fastify, {
-      issuer: { decorator: issuerDecorator, ...createIssuerOptions },
-      ...options
-    }) => {
-      const issuer = await openIDCreateIssuer.call(fastify, createIssuerOptions)
-      fastify.log.debug(issuer.metadata, 'OpenID issuer metadata')
-      if (issuerDecorator !== undefined) {
-        fastify.log.trace(
-          `decorating \`fastify[${String(issuerDecorator)}]\` with OpenID issuer`
-        )
-        fastify.decorate(issuerDecorator, issuer)
-      }
-      if (options.client !== undefined) {
-        const { decorator: clientDecorator, ...createClientOptions } = options.client
-        const client = openIDCreateClient.call(fastify, issuer, createClientOptions)
-        fastify.log.debug(client.metadata, 'OpenID client metadata')
-        fastify.log.trace(
-          `decorating \`fastify[${String(clientDecorator)}]\` with OpenID client`
-        )
-        fastify.decorate(clientDecorator, client)
-      }
-    },
-    {
-      fastify: '4.x',
-      name: 'fastify-openid-client'
-    }
-  )
+export const openIDClientPlugin: FastifyPluginAsync<FastifyOpenIDClientPluginOptions> = async (fastify, {
+  issuer: { decorator: issuerDecorator, ...createIssuerOptions },
+  ...options
+}) => {
+  const issuer = await openIDCreateIssuer.call(fastify, createIssuerOptions)
+  fastify.log.debug(issuer.metadata, 'OpenID issuer metadata')
+  if (issuerDecorator !== undefined) {
+    fastify.log.trace(
+      `decorating \`fastify[${String(issuerDecorator)}]\` with OpenID issuer`
+    )
+    fastify.decorate(issuerDecorator, issuer)
+  }
+  if (options.client !== undefined) {
+    const { decorator: clientDecorator, ...createClientOptions } = options.client
+    const client = openIDCreateClient.call(fastify, issuer, createClientOptions)
+    fastify.log.debug(client.metadata, 'OpenID client metadata')
+    fastify.log.trace(
+      `decorating \`fastify[${String(clientDecorator)}]\` with OpenID client`
+    )
+    fastify.decorate(clientDecorator, client)
+  }
+}
 
-export default openIDClientPlugin
+export default fp(openIDClientPlugin, {
+  fastify: '4.x',
+  name: 'fastify-openid-client'
+});
